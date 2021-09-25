dad280e2fa945007d5071a318ca5156525beb656
Nicolas Conte <53163360+Nicoconte@users.noreply.github.com>
2021-09-25 18:21:54 -0300

    Fix: Join the user into the room twice

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -3,8 +3,8 @@ const AuthTokenService = require('../services/authtoken-service')
 
 const RESPONSE_MESSAGE = require('../enums/response-message')
 
-const RoomController = function(req, res) {
-    
+const RoomController = function (req, res) {
+
     const roomService = RoomService();
     const authtokenService = AuthTokenService();
 
@@ -16,10 +16,10 @@ const RoomController = function(req, res) {
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-            
+
             let roomData = req?.body;
             roomData.userId = token.userId
-    
+
             roomService.createAsync(roomData).then(r => {
                 if (!r) {
                     return res.send({
@@ -27,13 +27,13 @@ const RoomController = function(req, res) {
                         "reason": RESPONSE_MESSAGE.CANNOT_CREATE('Room')
                     })
                 }
-    
+
                 return res.send({
                     "status": true,
                     "data": r
                 })
             })
-    
+
         }).catch(err => {
             return res.send({
                 "status": false,
@@ -44,29 +44,29 @@ const RoomController = function(req, res) {
     }
 
     async function listUserRoom() {
-        authtokenService.validateAsync(req.headers.authorization).then(async token => {            
+        authtokenService.validateAsync(req.headers.authorization).then(async token => {
             if (!token) {
                 return res.send({
                     "status": false,
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-    
+
             await roomService.listUserRoomsAsync(token.userId).then(rooms => {
                 return res.send({
                     "status": true,
                     "data": rooms
                 })
             })
-    
+
         })
-        .catch(err => {
-            return res.send({
-                "status": false,
-                "reason": RESPONSE_MESSAGE.UNEXPECTED_ERROR,
-                "innerReason": err
+            .catch(err => {
+                return res.send({
+                    "status": false,
+                    "reason": RESPONSE_MESSAGE.UNEXPECTED_ERROR,
+                    "innerReason": err
+                })
             })
-        })    
     }
 
     async function listPublicRoom() {
@@ -77,14 +77,14 @@ const RoomController = function(req, res) {
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-    
+
             await roomService.listPublicRoomsAsync().then(publicRooms => {
                 return res.send({
                     "status": true,
                     "data": publicRooms
                 })
             })
-    
+
         }).catch(err => {
             return res.send({
                 "status": false,
@@ -102,14 +102,14 @@ const RoomController = function(req, res) {
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-            
+
             await roomService.findByIdAsync(req?.params.id).then(room => {
                 return res.send({
                     "status": true,
                     "data": room
                 })
             })
-    
+
         }).catch(err => {
             return res.send({
                 "status": false,
@@ -127,18 +127,18 @@ const RoomController = function(req, res) {
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-    
+
             await roomService.findByIdAsync(req?.params?.id).then(async room => {
                 let userId = token.userId;
                 let roomId = room.id;
-    
+
                 if (await roomService.findUserAlreadyExist(roomId, userId)) {
                     return res.send({
                         "status": false,
                         "reason": RESPONSE_MESSAGE.USER_ALREADY_EXIST_IN_THE_ROOM
                     })
                 }
-                
+
                 //PRIVATE ROOM
                 if (!room.isPublic) {
                     if (req.body.password !== room.password) {
@@ -147,14 +147,14 @@ const RoomController = function(req, res) {
                             "reason": RESPONSE_MESSAGE.PASSWORD_DOES_NOT_MATCH
                         })
                     }
-                    
+
                     if (await roomService.getParticipantCountByIdAsync(roomId) > room.maxNumberOfParticipants) {
                         return res.send({
                             "status": false,
                             "reason": RESPONSE_MESSAGE.NOT_ENOUGH_SPACE
                         })
                     }
-    
+
                     await roomService.assignUserToRoomAsync(roomId, userId).then(_ => {
                         return res.send({
                             "status": true,
@@ -163,18 +163,18 @@ const RoomController = function(req, res) {
                             }
                         })
                     });
-                }
-                
 
-                //PUBLIC ROOM
-                await roomService.assignUserToRoomAsync(roomId, userId).then(_ => {
-                    return res.send({
-                        "status": true,
-                        "data":{
-                            "message": RESPONSE_MESSAGE.USER_ADDED(userId)
-                        }
+                } else {
+                    //PUBLIC ROOM
+                    await roomService.assignUserToRoomAsync(roomId, userId).then(_ => {
+                        return res.send({
+                            "status": true,
+                            "data": {
+                                "message": RESPONSE_MESSAGE.USER_ADDED(userId)
+                            }
+                        })
                     })
-                })
+                }
             })
         })
     }
@@ -190,11 +190,11 @@ const RoomController = function(req, res) {
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-    
+
             await roomService.findByIdAsync(req?.params?.id).then(async room => {
                 let userId = token.userId;
                 let roomId = room.id;
-                
+
                 if (!room.isPublic) {
                     if (req.body.password !== room.password) {
                         return res.send({
@@ -202,14 +202,14 @@ const RoomController = function(req, res) {
                             "reason": RESPONSE_MESSAGE.PASSWORD_DOES_NOT_MATCH
                         })
                     }
-    
+
                     if (!await roomService.findUserAlreadyExist(roomId, userId)) {
                         return res.send({
                             "status": true,
                             "reason": "Yo no estoy dentro de la sala"
                         })
                     }
-                    
+
                     return res.send({
                         "status": true,
                         "canEmit": true
@@ -221,14 +221,14 @@ const RoomController = function(req, res) {
                             "status": false,
                             "reason": "You're not a member of this room"
                         })
-                    }    
-                    
+                    }
+
                     return res.send({
                         "status": true,
                         "canEmit": true
                     });
                 }
-            })    
+            })
         })
     }
 
@@ -240,27 +240,27 @@ const RoomController = function(req, res) {
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-    
+
             await roomService.deleteAsync(req?.params?.id, token.userId)
-            .then(r => {
-                if (!r) {
+                .then(r => {
+                    if (!r) {
+                        return res.send({
+                            "status": false,
+                            "reason": RESPONSE_MESSAGE.CANNOT_DELETE
+                        })
+                    }
+
                     return res.send({
-                      "status": false,
-                      "reason": RESPONSE_MESSAGE.CANNOT_DELETE  
+                        "status": true
                     })
-                }
-    
-                return res.send({
-                    "status": true
                 })
-            })
-            .catch(err => {
-                return res.send({
-                    "status": false,
-                    "reason": RESPONSE_MESSAGE.ROOM_DOES_NOT_EXIST
-                })
-            });
-        
+                .catch(err => {
+                    return res.send({
+                        "status": false,
+                        "reason": RESPONSE_MESSAGE.ROOM_DOES_NOT_EXIST
+                    })
+                });
+
         }).catch(err => {
             return res.send({
                 "status": false,
@@ -278,27 +278,27 @@ const RoomController = function(req, res) {
                     "reason": RESPONSE_MESSAGE.INVALID_TOKEN
                 })
             }
-    
+
             await roomService.updateAsync(req?.params?.id, token.userId, req?.body.data)
-            .then(r => {
-                if (!r) {
+                .then(r => {
+                    if (!r) {
+                        return res.send({
+                            "status": false,
+                            "reason": RESPONSE_MESSAGE.CANNOT_UPDATE
+                        })
+                    }
+
+                    return res.send({
+                        "status": true
+                    })
+                })
+                .catch(err => {
                     return res.send({
                         "status": false,
-                        "reason": RESPONSE_MESSAGE.CANNOT_UPDATE
+                        "reason": RESPONSE_MESSAGE.ROOM_DOES_NOT_EXIST
                     })
-                }
-    
-                return res.send({
-                    "status": true
                 })
-            })
-            .catch(err => {
-                return res.send({
-                    "status": false,
-                    "reason": RESPONSE_MESSAGE.ROOM_DOES_NOT_EXIST
-                })
-            })
-    
+
         }).catch(err => {
             return res.send({
                 "status": false,
@@ -318,13 +318,13 @@ const RoomController = function(req, res) {
             }
 
             let room = await roomService.findByInviteCode(req?.params?.code);
-            
+
 
             if (!room) {
                 return res.send({
                     "status": false,
                     "data": RESPONSE_MESSAGE.ROOM_DOES_NOT_EXIST
-                })                
+                })
             }
 
             return res.send({
@@ -338,8 +338,8 @@ const RoomController = function(req, res) {
                 "reason": RESPONSE_MESSAGE.UNEXPECTED_ERROR,
                 "innerReason": err.toString()
             })
-        }) 
-    } 
+        })
+    }
 
 
     async function removeParticipant() {
@@ -352,19 +352,19 @@ const RoomController = function(req, res) {
             }
 
             roomService.deleteParticipant(req?.params?.id, req?.params?.userId)
-            .then(r => {
-                if (r) {
-                    return res.send({
-                        "status": true
-                    })
-                }
-            })
-            .catch(err => {
-                return res.send({
-                    "status": false,
-                    "reason": RESPONSE_MESSAGE.CANNOT_DELETE
+                .then(r => {
+                    if (r) {
+                        return res.send({
+                            "status": true
+                        })
+                    }
                 })
-            })
+                .catch(err => {
+                    return res.send({
+                        "status": false,
+                        "reason": RESPONSE_MESSAGE.CANNOT_DELETE
+                    })
+                })
 
         }).catch(err => {
             return res.send({
